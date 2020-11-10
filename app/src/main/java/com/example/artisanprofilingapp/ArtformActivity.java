24b9f52c9762726345b57bc55a2d95e2a02fdfa7
Swarup Dey Sarkar <deysarkarswarup123@gmail.com>
2020-11-11 00:33:47 +0530

    fixing bugs

--- app/src/main/java/com/example/artisanprofilingapp/ArtformActivity.java
+++ app/src/main/java/com/example/artisanprofilingapp/ArtformActivity.java
@@ -17,6 +17,7 @@ import android.widget.EditText;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 import com.android.volley.Request;
@@ -36,31 +37,28 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class ArtformActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
-    TextInputLayout artform;
+public class ArtformActivity extends AppCompatActivity {
+    TextInputLayout experi;
     EditText nam;//to show error msg
     Button submitbtn;
     RequestQueue requestQueue;
     ProgressDialog progressDialog;
-    String ArtformHolder;
+    String DataHolder;
     SharedPreferences myPref;
-    private MediaPlayer mediaPlayer;
-    String[] artFormName = new String[10];
-    Spinner spin;
-    Boolean flag = false;
+    //    private MediaPlayer mediaPlayer;
+    RadioGroup radioGroup, radioGroup1, radioGroup2;
+    String yesOrNo, yesOrNo1,typeOfSilpo;
+//    int potaka=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artform);
 
-        //artform = (TextInputLayout)findViewById(R.id.artform);
-        spin = (Spinner) findViewById(R.id.spinner);
-        nam = (EditText)findViewById(R.id.nam);//to show error msg
-        submitbtn = (Button)findViewById(R.id.submitBtn);
-        mediaPlayer = MediaPlayer.create(this, R.raw.artforminst);
+        experi = findViewById(R.id.experi);
+        nam = findViewById(R.id.nam);//to show error msg
 
-        mediaPlayer.start();
+        submitbtn = findViewById(R.id.submitBtn);
 
         //Initialize of SharedPref
         myPref = getApplicationContext().getSharedPreferences("MyPref",MODE_PRIVATE);
@@ -68,24 +66,105 @@ public class ArtformActivity extends AppCompatActivity implements AdapterView.On
         // Creating Volley newRequestQueue .
         requestQueue = Volley.newRequestQueue(ArtformActivity.this);
         progressDialog = new ProgressDialog(ArtformActivity.this);
-        getData();
+//        mediaPlayer = MediaPlayer.create(this, R.raw.addressinst);
+//
+//        mediaPlayer.start();
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup1);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.yes:
+                        yesOrNo = "হ্যাঁ";
+//                        potaka++;
+                        break;
+                    case R.id.no:
+                        yesOrNo = "না";
+//                        potaka++;
+                        break;
+                }
+            }
+        });
+
+        radioGroup2 = (RadioGroup) findViewById(R.id.radioGroup2);
+        radioGroup2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.radio1:
+                        typeOfSilpo = "কাঁথাস্টিচ";
+//                        potaka++;
+                        break;
+                    case R.id.radio2:
+                        typeOfSilpo = "বাটিক প্রিন্ট";
+//                        potaka++;
+                        break;
+                    case R.id.radio3:
+                        typeOfSilpo = "চামড়ার কাজ";
+//                        potaka++;
+                        break;
+                    case R.id.radio4:
+                        typeOfSilpo = "জুয়েলারি";
+//                        potaka++;
+                        break;
+                    case R.id.radio5:
+                        typeOfSilpo = "অন্যান্য শিল্প কর্ম";
+//                        potaka++;
+                        break;
+                }
+            }
+        });
+
+        radioGroup1 = (RadioGroup) findViewById(R.id.radioGroup1);
+        radioGroup1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.yes1:
+                        yesOrNo1 = "হ্যাঁ";
+//                        potaka++;
+                        break;
+                    case R.id.no1:
+                        yesOrNo1 = "না";
+//                        potaka++;
+                        break;
+
+                }
+            }
+        });
 
         submitbtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+
+
                 ConnectivityManager con = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
                 NetworkInfo networkInfo = con.getActiveNetworkInfo();
                 if (networkInfo != null && networkInfo.isConnected()) {
-                    if (flag) {
-                        //Toast.makeText(MainActivity.this,"হয়েগেছে",Toast.LENGTH_LONG).show();
-                        //regUser();
-                        mediaPlayer.stop();
-                        Intent i = new Intent(ArtformActivity.this, ArtformActivity2.class);
+                    if (!nam.getText().toString().equals("")) {
+//                        potaka++;
+                        regUser();
+////                        mediaPlayer.stop();
+                        Intent i = new Intent(ArtformActivity.this, DirectionToCaptureImageActivity.class);
                         startActivity(i);
+
                     } else {
-                        nam.setError("আপনি কোন শিল্প নিয়ে কাজ করেন?");
+                        nam.setError("টাইপ করুন");
                     }
+
+//                    if (potaka==4){
+//                        regUser();
+////                        mediaPlayer.stop();
+//                        Intent i = new Intent(ArtformActivity.this, ProfilePicActivity.class);
+//                        startActivity(i);
+//                    }
+//                    else {
+//                        Log.d("potaka", String.valueOf(potaka));
+//                        Toast.makeText(ArtformActivity.this,"সব ফিল্ড টাইপ করুন",Toast.LENGTH_LONG).show();
+//                    }
                 }
+
                 else{
                     Intent intent = new Intent(ArtformActivity.this, InternetCheckActivity.class);
                     startActivity(intent);
@@ -93,161 +172,66 @@ public class ArtformActivity extends AppCompatActivity implements AdapterView.On
                 }
             }
 
+            private void regUser() {
+                progressDialog.setMessage("Please Wait, We are Inserting Your Data on Server");
+                progressDialog.show();
 
+                DataHolder = experi.getEditText().getText().toString().trim();
+
+                String idToGet = myPref.getString("id","No data found");
+
+                DataHolder = DataHolder.replaceAll(" ","%20");
+
+                idToGet = idToGet.replaceAll(" ","%20");
+
+                String characterFilter = "[^\\p{L}\\p{M}\\p{N}\\p{P}\\p{Z}\\p{Cf}\\p{Cs}\\s]";
+                DataHolder = DataHolder.replaceAll(characterFilter,"");
+
+                idToGet = idToGet.replaceAll(characterFilter,"");
+
+                String myurl = "https://artisanapp.xyz/newartform.php?typeOfSilpo=" + typeOfSilpo+"&hascoworker="+ yesOrNo
+                        +"&experience="+DataHolder+"&orgmember="+yesOrNo1 +"&id="+idToGet;
+
+
+
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, myurl,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String ServerResponse) {
+                                // Hiding the progress dialog after all task complete.
+                                progressDialog.dismiss();
+                                // Showing response message coming from server.
+                                Toast.makeText(ArtformActivity.this, ServerResponse, Toast.LENGTH_LONG).show();
+                                myPref.edit().putString("track", "4").apply();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError volleyError) {
+                                // Hiding the progress dialog after all task complete.
+                                progressDialog.dismiss();
+                                // Showing error message if something goes wrong.
+                                Toast.makeText(ArtformActivity.this, volleyError.toString(), Toast.LENGTH_LONG).show();
+                            }
+                        });
+                queue.add(stringRequest);
+
+            }
 
         });
 
-    }
-
-    private void getData() {
-//        String url ="http://192.168.43.12/Artisans-Profiling/artform.php";
-        String url ="https://artisanapp.xyz/artform.php";
-
-        StringRequest stringRequest = new StringRequest(url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                myPref.edit().putString("track", "5").apply();
-                showJSON(response);
-            }
-        },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(ArtformActivity.this, error.getMessage().toString(), Toast.LENGTH_LONG).show();
-                    }
-                });
-
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
 
     }
-
-    private void showJSON(String response)  {
-        ArrayList<HashMap<String, String>> list = new ArrayList<>();
-        try {
-            JSONObject jsonObject = new JSONObject(response);
-            JSONArray result = jsonObject.getJSONArray(Config.JSON_ARRAY);
-
-            for (int i = 0; i < result.length(); i++) {
-                JSONObject jo = result.getJSONObject(i);
-//                String iname = jo.getString(Config5.IName);
-//                String itel = jo.getString(Config5.Itel);
-//                String data = jo.getString(Config5.KEY_DATA);
-                //String id = jo.getString(Config5.KEY_ID);
-                artFormName[i] = jo.getString(Config.ArtFormName);
-                Log.d("hmm i->", String.valueOf(i));
-                Log.d("hmm id->",artFormName[i]);
-//            Log.d("hmm id->",id);
-//                Log.d("hmm iname->",iname);
-//                Log.d("hmm itel->",itel);
-
-
-
-                final HashMap<String, String> artfrm = new HashMap<>();
-                artfrm.put(Config.ArtFormName, artFormName[i]);
-//                employees.put(Config5.IName,  iname);
-//                employees.put(Config5.Itel, itel);
-////                employees.put(Config5.KEY_DATA, data);
-//                employees.put(Config5.KEY_ID, id);
-
-                list.add(artfrm);
-
-            }
-
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-//        ListAdapter adapter = new SimpleAdapter(
-//                MainActivity.this, list, R.layout.activity_mylist,
-////                new String[]{Config5.IName,Config5.Itel,Config5.KEY_ID},
-////                new int[]{R.id.iname, R.id.itel,R.id.tvid});
-//
-//                new String[]{Config5.ArtFormName},
-//                new int[]{R.id.tvid});
-//        Log.d("hmm listview", Arrays.toString(artFormName));
-//       listview.setAdapter(adapter);
-        ArrayList<String> list1 = new ArrayList<String>();
-        int j=0;
-
-        for(int i=0;i<artFormName.length;i++){
-            if (artFormName[i]!=null) {
-                list1.add(artFormName[i]);
-//                Log.d("dekha", "showJSON: " + art[j - 1]);
-            }
-        }
-        artFormName = list1.toArray(new String[list1.size()]);
-
-        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, R.layout.spinner_layout, artFormName);
-        adapter1.setDropDownViewResource(R.layout.spinner_layout);
-//        ArrayAdapter adapter1 = ArrayAdapter.createFromResource(this,artFormName,R.layout.spinner_layout);
-        spin.setAdapter(adapter1);
-        spin.setOnItemSelectedListener(this);
-
-    }
-    public void onItemSelected(AdapterView<?> arg0, View arg1, int position,long id) {
-       // Toast.makeText(getApplicationContext(), "Selected User: "+artFormName[position] ,Toast.LENGTH_SHORT).show();
-        myPref.edit().putString("artform",artFormName[position]).apply();
-        flag = true;
-
-    }
-    @Override
-    public void onNothingSelected(AdapterView<?> arg0) {
-        // TODO - Custom Code
-    }
-
-
-//    private void regUser() {
-//
-//            progressDialog.setMessage("Please Wait, We are Inserting Your Data on Server");
-//            progressDialog.show();
-//
-//            ArtformHolder = artform.getEditText().getText().toString().trim();
-//            Log.d("eirki",ArtformHolder);
-//            //myPref.edit().putString("phone", PhoneNoHolder).apply();
-//            String dataToGet = myPref.getString("phone","No data found");
-//            Log.d("eirki",dataToGet);
-//
-//            String myurl = "http://192.168.43.12/Artisans-Profiling/artform.php?artform=" + ArtformHolder +"&phoneno="+ dataToGet;
-//
-//
-//
-//            RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-//            StringRequest stringRequest = new StringRequest(Request.Method.POST, myurl,
-//                    new Response.Listener<String>() {
-//                        @Override
-//                        public void onResponse(String ServerResponse) {
-//                            // Hiding the progress dialog after all task complete.
-//                            progressDialog.dismiss();
-//
-//                            // Showing response message coming from server.
-//                            Toast.makeText(ArtformActivity.this, ServerResponse, Toast.LENGTH_LONG).show();
-//                            myPref.edit().putString("track", "4").apply();
-//                        }
-//                    },
-//                    new Response.ErrorListener() {
-//                        @Override
-//                        public void onErrorResponse(VolleyError volleyError) {
-//                            // Hiding the progress dialog after all task complete.
-//                            progressDialog.dismiss();
-//                            // Showing error message if something goes wrong.
-//                            Toast.makeText(ArtformActivity.this, volleyError.toString(), Toast.LENGTH_LONG).show();
-//
-//                        }
-//                    });
-//            queue.add(stringRequest);
-//
-//
-//    }
 
     @Override
     public void onBackPressed() {
-        mediaPlayer.stop();
+//        mediaPlayer.stop();
         super.onBackPressed();
     }
     @Override
     public void onUserLeaveHint(){
-        mediaPlayer.stop();
+//        mediaPlayer.stop();
         super.onUserLeaveHint();
     }
 }
